e245199be3f44271cddb693cf7fb3b93866bf8a4
Ekel Leal <ekelleal@gmail.com>
2023-10-17 20:07:08 -0300

    creacion ecommerce curso NextJS
    primer commit creo rutas y layout

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const about = () => {
+  return (
+    <div className='p-8'>
+      <h1 className="text-4xl py-4">Page About</h1>
+    </div>
+  )
+}
+
+export default about
